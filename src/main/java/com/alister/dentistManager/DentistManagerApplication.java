37053513ac300dbadb1d1f9b004b57236d72e33e
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2023-10-07 16:23:06 +0200

    Firts commit

---
+++ src/main/java/com/alister/dentistManager/DentistManagerApplication.java
@@ -0,0 +1,13 @@
+package com.alister.dentistManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentistManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentistManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alister/dentistManager/DentistManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alister.dentistManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentistManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
